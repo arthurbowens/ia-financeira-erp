66fe6a820652debbfbde606163fc548dd9531ac4
arthurbowens <arthurbowens18@gmail.com>
2025-10-03 13:29:37 -0300

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  safelist: [
+    'border-green-500',
+    'border-yellow-500',
+    'text-green-600',
+    'bg-green-50',
+    'text-red-600',
+    'border-blue-500',
+    'border-cyan-500',
+    'border-indigo-500',
+    'border-fuchsia-500',
+    'border-pink-500',
+    'border-orange-500',
+    'border-teal-500',
+    'border-purple-500',
+    'border-rose-500',
+    'border-amber-500',
+    'border-sky-500'
+  ],
   theme: {
     extend: {
       colors: {
